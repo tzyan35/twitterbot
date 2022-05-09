beeba67e0580cd5380eb5100c0c43c034994599a
tzyan35 <zhongyan1997@hotmail.com>
2022-05-10 05:04:19 +0800

    insert API keys

--- config_sample.js
+++ config_sample.js
@@ -1,8 +1,8 @@
 // Put your own Twitter App keys here. See README.md for more detail.
 module.exports = {
-    consumer_key:         'API key',
-    consumer_secret:      'API secret key',
-    access_token:         'Access token',
-    access_token_secret:  'Access token secret'
+    consumer_key:         '2gA9C3Gu0IjMJ3pS6fLBCvfTL',
+    consumer_secret:      'rSepnyKQpbX8HBbsSLmibxVC9y6evxaDSZxry3lSQrIQCTbBJe',
+    access_token:         '1480537484710662145-lPb9fsKyh1e369GSrDhADR86aNl0v1',
+    access_token_secret:  '1480537484710662145-lPb9fsKyh1e369GSrDhADR86aNl0v1'
   }
   
