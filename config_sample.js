a1b6b84dc1b1d3f3ec48a854a1086694b22fb9c8
tzyan35 <zhongyan1997@hotmail.com>
2022-05-10 05:05:38 +0800

    changed folder

--- config_sample.js
+++ config_sample.js
@@ -1,8 +1,1 @@
 // Put your own Twitter App keys here. See README.md for more detail.
-module.exports = {
-    consumer_key:         '2gA9C3Gu0IjMJ3pS6fLBCvfTL',
-    consumer_secret:      'rSepnyKQpbX8HBbsSLmibxVC9y6evxaDSZxry3lSQrIQCTbBJe',
-    access_token:         '1480537484710662145-lPb9fsKyh1e369GSrDhADR86aNl0v1',
-    access_token_secret:  '1480537484710662145-lPb9fsKyh1e369GSrDhADR86aNl0v1'
-  }
-  
